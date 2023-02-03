39f89d2c89b47975a7c9a846717b5b94f7f46995
ajp109 <andrew.pomfret@york.ac.uk>
2023-02-03 20:20:15 +0000

    Add project-specific files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include "mbed.h"
 
 int main() {
+
+    char array[] = "Hello!";
+    char const * literal = "Hello!";
+
+    printf("array: %s\n", array);
+    printf("literal: %s\n", literal);
+
+    printf("array is stored at %p\n", array);
+    printf("literal is stored at %p\n", literal);
+
     while (true) {
         sleep();
     }
